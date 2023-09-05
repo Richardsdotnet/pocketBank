942edbed619fb8a17208797a39dab6b52c93a948
Akaabiam Richards <richardsakaabiam@gmail.com>
2023-09-05 11:21:14 +0100

    update

---
+++ src/main/java/com/richards/pocketBank/PocketBankApplication.java
@@ -0,0 +1,13 @@
+package com.richards.pocketBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richards/pocketBank/PocketBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richards.pocketBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
